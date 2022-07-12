cc51bc9f429c8687611ad83c4a45a794bd748eb1
Luong Trung <trunghlbuh@gmail.com>
2022-07-13 01:40:13 +0700

    Init source

---
+++ project/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mydatabase
+export POSTGRES_PASSWORD=password123
+export POSTGRES_HOST=database-796706398716.cuhvwdf5wa2e.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:rds:ap-southeast-1:796706398716:db:database-796706398716
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
